7d14fee09d97ff5528781a1bb53304caae592eba
OlivandoGit <oliver.lister2002@gmail.com>
2021-12-10 02:24:06 +0000

    Refactored Day 6

--- Day 06/Part 1.py
+++ Day 06/Part 1.py
@@ -1,8 +1,7 @@
 ages = [0 for i in range(9)]
 
 with open("Input.txt", "r") as f:
-    temp = f.readline().strip("\n").split(",")
-    temp = [int(i) for i in temp]
+    temp = [int(i) for i in f.readline().strip("\n").split(",")]
 
     for tmp in temp:
         ages[tmp] += 1
@@ -11,7 +10,7 @@ with open("Input.txt", "r") as f:
 
 for i in range(80):
     temp = ages.pop(0)
-    ages[6] = ages[6] + temp
+    ages[6] += temp
     ages.append(temp)
 
 print(sum(ages))

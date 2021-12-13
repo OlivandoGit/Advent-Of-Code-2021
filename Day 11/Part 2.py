211c5b1faf7105d1b85ad98e9838ec67cf789820
OlivandoGit <oliver.lister2002@gmail.com>
2021-12-13 22:29:03 +0000

    Completed Day 11

---
+++ Day 11/Part 2.py
@@ -0,0 +1,44 @@
+map = []
+
+with open("Input.txt", "r") as f:
+    map = [[int(char) for char in list(line.strip("\n"))] for line in f]
+    f.close()
+
+step = 0
+while True:
+    step += 1
+
+    for y, row in enumerate(map):
+        for x, r in enumerate(row):
+            map[y][x] += 1
+
+    flashed = []
+    for y, row in enumerate(map):
+        for x, r in enumerate(row):
+            if not r > 9 or (x, y) in flashed:
+                continue
+
+            flashed.append((x, y))
+
+    c = 0
+    for flash in flashed:
+        x, y = tuple(flash)
+        c += 1
+
+        for dy in range(-1, 2):
+            for dx in range(-1, 2):
+                if y + dy >= 0 and y + dy < len(map) and x + dx >= 0 and x + dx < len(row):
+                    map[y + dy][x + dx] += 1
+
+                    if map[y + dy][x + dx] > 9 and (x + dx, y + dy) not in flashed:
+                        flashed.append((x + dx, y + dy))
+
+    if c == len(map[0]) * len(map):
+        break
+
+    for y, row in enumerate(map):
+        for x, r in enumerate(row):
+            if r > 9:
+                map[y][x] = 0
+
+print(step)
